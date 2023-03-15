313c7047f34379b9ad0e14109004dac6c8f9bb6b
wangfanghao <1145086558@qq.com>
2023-03-15 16:08:04 +0800

    JSON.toJSONString()参数为对象，对象中的空值默认不参与序列化

--- src/main/java/com/test/TOJsonString.java
+++ src/main/java/com/test/TOJsonString.java
@@ -1,6 +1,7 @@
 package com.test;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.pojo.Student;
 
 import java.util.Date;
@@ -11,7 +12,10 @@ public class TOJsonString {
         student.id = 1;
         student.name = "123";
         student.birthday = getData();
-        String jsonString = JSON.toJSONString(student);
+        student.email = null;
+
+        //  JSON.toJSONString()参数为对象，对象中的空值默认不参与序列化
+        String jsonString = JSON.toJSONString(student, SerializerFeature.WriteMapNullValue);
         System.out.println(jsonString);
     }
 
